28d88a426e4b6ebea8a4605db3c2aa9a995d9c62
AliceUwase <a.uwase3@alustudent.com>
2026-09-25 23:39:52 +0200

    refactor: replace local theme state with useTheme

--- react-state-management/src/components/Navbar.tsx
+++ react-state-management/src/components/Navbar.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import styles from "./Navbar.module.css";
+import { useTheme } from "../context/ThemeContext";
 import { DARK_THEME, LIGHT_THEME } from "../constants/theme";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(LIGHT_THEME);
-  const toggleTheme = () => {
-    setTheme((currentTheme) =>
-      currentTheme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME,
-    );
-  };
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <nav className={styles.navbar}>
       <span className={styles.brand}>React App</span>
+
       <button className={styles.toggleButton} onClick={toggleTheme}>
         Switch to {theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME} Mode
       </button>
